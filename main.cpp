993d9b6a6d50dfbc9ae60ded31de77269d080ce7
QuangHai <35519639+QuangHaiDo@users.noreply.github.com>
2018-09-12 23:27:45 +0700

    Nop Bai

---
+++ main.cpp
@@ -0,0 +1,33 @@
+ #include <windows.h>
+  #include "GL/glut.h"
+
+void displayMe(void)
+{
+    // Lệnh xóa nền
+    glClear(GL_COLOR_BUFFER_BIT);
+
+
+    // Bắt đầu vẽ đa giác
+    glBegin(GL_POLYGON);
+        // Các đỉnh được liệt kê sẽ tự nối với nhau và đỉnh cuối sẽ nôi với đỉnh đầu
+        glVertex3f(0.5, 0.0, 0.5);
+        glVertex3f(0.0, 0.5, 0.0);
+        glVertex3f(0.0, 0.0, 0.5);
+        glVertex3f(0.5, 0.5, 0.5);
+    glEnd();
+    // Loại bỏ các transforms
+    glFlush();
+
+}
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE);
+    glutInitWindowSize(400, 300);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Hello world!");
+    glutDisplayFunc(displayMe);
+    glutMainLoop();
+    return 0;
+}
